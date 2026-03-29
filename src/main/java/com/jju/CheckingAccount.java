4e49f3bdcfbdd9009a7965b20eb38d471e7ea832
mahamedamin232-wq <mahamedamin232@gmail.com>
2026-03-29 23:06:05 +0300

    Implement constructor for CheckingAccount
    Added constructor to initialize CheckingAccount and call superclass constructor.

--- src/main/java/com/jju/CheckingAccount.java
+++ src/main/java/com/jju/CheckingAccount.java
@@ -3,12 +3,26 @@ package com.jju;
 public class CheckingAccount extends BankAccount {
     private static final double TRANSACTION_FEE = 1.50;
 
-  // 1. Student Task: Create constructor and call the superclass constructor and ensure balance is valid
-    
-
+      public CheckingAccount(String accountHolder, double initialBalance) {
+        super(accountHolder, initialBalance); 
+        }
     @Override
     public void withdraw(double amount) {
        // 2. Student Task: Implement withdraw logic here
+               if (amount <= 0) {
+            throw new IllegalArgumentException("Withdrawal amount must be positive.");
+        }
+
+        double totalAmount = amount + TRANSACTION_FEE;
+
+        
+        if (totalAmount > balance) {
+            throw new IllegalStateException("Insufficient balance (including transaction fee).");
+        }
+
+        
+        super.withdraw(totalAmount);
+    
     }
 
     
